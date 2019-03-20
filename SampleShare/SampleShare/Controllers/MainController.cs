695b3fb2fc17243f1a772a49f50c688a559322c4
Jannick Jensen <jann4286@elev.tec.dk>
2019-03-20 10:24:38 +0100

    Login knap og convert til Bootstrap.

--- SampleShare/SampleShare/Controllers/MainController.cs
+++ SampleShare/SampleShare/Controllers/MainController.cs
@@ -44,6 +44,11 @@ namespace SampleShare.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
